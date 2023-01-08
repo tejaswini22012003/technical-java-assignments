df28a7c20dfab22c5911a8bfa6d81622c9515edc
tejaswinipeerugouda <96379564+tejaswini22012003@users.noreply.github.com>
2023-01-08 23:02:56 +0530

    Add files via upload

---
+++ reverse_no.java
@@ -0,0 +1,22 @@
+
+//reversing a number
+
+import java.util.Scanner;
+
+public class reverse_no {
+    public static void main(String[] args) {
+        int rev = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number : ");
+        int n = sc.nextInt();
+        int num = n;
+        while (n != 0) {
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n = n / 10;
+        }
+        System.out.println("the reversal of " + num + " is = " + rev);
+    }
+
+}
