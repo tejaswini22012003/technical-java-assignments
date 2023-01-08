020241b34b777d7ff6ac347b5413e19d11724232
tejaswinipeerugouda <96379564+tejaswini22012003@users.noreply.github.com>
2023-01-09 00:37:01 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String args[]) {
+        int len = new java.util.Scanner(System.in).nextInt();
+        char c[] = new char[len];
+        for (int i = 0; i < len; i++)
+            c[i] = (char) ('A' + i);
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            if (i == len - 1)
+                System.out.println(" ".repeat(i) + c[index]);
+            else
+                System.out.println(" ".repeat(i) + c[index] + " ".repeat((len - i) * 2 - 3) + c[index++]);
+        }
+    }
+}
