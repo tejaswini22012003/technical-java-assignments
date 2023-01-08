572ec7e128e1906705c0ab7052150d6cc042f32c
tejaswinipeerugouda <96379564+tejaswini22012003@users.noreply.github.com>
2023-01-08 23:01:01 +0530

    Add files via upload

---
+++ fibonacci_series.java
@@ -0,0 +1,25 @@
+
+//fibonacci series
+import java.util.*;
+
+public class fibonacci_series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a number : ");
+        int n = sc.nextInt();
+        int f1 = 0;
+        int f2 = 1;
+        System.out.println("fibonacci series :");
+        System.out.print(f1 + " " + f2 + " ");
+        for (int i = 2; i < n; i++) {
+            int f3 = f1 + f2;
+
+            System.out.print(f3 + " ");
+
+            f1 = f2;
+            f2 = f3;
+        }
+
+    }
+}
